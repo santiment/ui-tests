ae9c74ab4bf95ba297e992b25068f49641bfaf39
Maksim Tkachuk <maksim.t@santiment.net>
2019-09-24 22:10:45 +0300

    Test fixes

--- features/discord_bot.py
+++ features/discord_bot.py
@@ -6,25 +6,25 @@ import datetime
 from discord import Webhook, RequestsWebhookAdapter, File
 from constants import DISCORD_WEBHOOK, ENVIRONMENT
 
+if DISCORD_WEBHOOK:
+    baseURL = "https://discordapp.com/api/webhooks/{}".format(DISCORD_WEBHOOK)
+    webhook = Webhook.from_url(baseURL, adapter=RequestsWebhookAdapter())
 
-baseURL = "https://discordapp.com/api/webhooks/{}".format(DISCORD_WEBHOOK)
-webhook = Webhook.from_url(baseURL, adapter=RequestsWebhookAdapter())
+    path_to_report = os.path.join('reports', '*.html')
+    files = [File(open(path, 'rb'), os.path.basename(path)) for path in glob.glob(path_to_report)]
 
-path_to_report = os.path.join('reports', '*.html')
-files = [File(open(path, 'rb'), os.path.basename(path)) for path in glob.glob(path_to_report)]
+    str_start = sys.argv[1]
+    str_finish = sys.argv[2]
+    pattern = '%H:%M:%S'
+    diff = datetime.datetime.strptime(str_finish, pattern) - datetime.datetime.strptime(str_start, pattern) 
+    str_diff = str(diff)
 
-str_start = sys.argv[1]
-str_finish = sys.argv[2]
-pattern = '%H:%M:%S'
-diff = datetime.datetime.strptime(str_finish, pattern) - datetime.datetime.strptime(str_start, pattern) 
-str_diff = str(diff)
-
-message = f"""
-++++++++++++++++++++++++++++++++++++++++++++++
-Test run results:
-Started at {str_start}, finished at {str_finish}
-Total duration: {str_diff}
-Environment: {ENVIRONMENT}
-===============================================
-"""
-webhook.send(message, username='TestResultsBot', files=files)
+    message = f"""
+    ++++++++++++++++++++++++++++++++++++++++++++++
+    Test run results:
+    Started at {str_start}, finished at {str_finish}
+    Total duration: {str_diff}
+    Environment: {ENVIRONMENT}
+    ===============================================
+    """
+    webhook.send(message, username='TestResultsBot', files=files)
