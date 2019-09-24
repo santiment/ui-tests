16d0afe9c9ebd6143e30086ecdc2e95d9fd61c8c
Maksim Tkachuk <maksim.t@santiment.net>
2019-09-24 15:35:14 +0300

    Post reports to discord

--- features/run-tests.sh
+++ features/run-tests.sh
@@ -1,6 +1,10 @@
 rm -r ./reports
+start=$(date +"%T")
+SECONDS=0
 behave $1
+finish=$(date +"%T")
 for entry in "./reports"/*.xml
 do
   junit2html "$entry" "${entry%.*}.html"
 done
+python discord_bot.py $start $finish $SECONDS
