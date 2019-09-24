4fa8dae8ab6f18168038ee7b683807443e86714a
Maksim Tkachuk <maksim.t@santiment.net>
2019-09-24 15:45:46 +0300

    Improvements based on review

--- features/run-tests.sh
+++ features/run-tests.sh
@@ -1,10 +1,10 @@
 rm -r ./reports
-start=$(date +"%T")
+START=$(date +"%T")
 SECONDS=0
 behave $1
-finish=$(date +"%T")
+FINISH=$(date +"%T")
 for entry in "./reports"/*.xml
 do
   junit2html "$entry" "${entry%.*}.html"
 done
-python discord_bot.py $start $finish $SECONDS
+python discord_bot.py $START $FINISH $SECONDS
