e0486a1e446277b0fbc6a043aa621c339a2fe91c
mtkachuk <maksim.tkachuk@gmail.com>
2019-09-27 14:22:33 +0300

    Add config to discord message

--- features/discord_bot.py
+++ features/discord_bot.py
@@ -4,7 +4,7 @@ import glob
 import sys, os
 import datetime
 from discord import Webhook, RequestsWebhookAdapter, File
-from constants import DISCORD_WEBHOOK, ENVIRONMENT
+from constants import DISCORD_WEBHOOK, ENVIRONMENT, CONFIG_FILE
 
 if DISCORD_WEBHOOK:
     baseURL = "https://discordapp.com/api/webhooks/{}".format(DISCORD_WEBHOOK)
@@ -16,7 +16,7 @@ if DISCORD_WEBHOOK:
     str_start = sys.argv[1]
     str_finish = sys.argv[2]
     pattern = '%H:%M:%S'
-    diff = datetime.datetime.strptime(str_finish, pattern) - datetime.datetime.strptime(str_start, pattern) 
+    diff = datetime.datetime.strptime(str_finish, pattern) - datetime.datetime.strptime(str_start, pattern)
     str_diff = str(diff)
 
     message = f"""
@@ -25,6 +25,7 @@ if DISCORD_WEBHOOK:
     Started at {str_start}, finished at {str_finish}
     Total duration: {str_diff}
     Environment: {ENVIRONMENT}
+    Config: {CONFIG_FILE}
     ===============================================
     """
     webhook.send(message, username='TestResultsBot', files=files)
